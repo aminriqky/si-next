2f4eb5461b410b4ee1b6b879a4abe6e07adeffa6
aminriqky <aminriqky@gmail.com>
2024-11-26 14:20:10 +0700

    5.4.1

--- pages/galeri/index.tsx
+++ pages/galeri/index.tsx
@@ -1,36 +1,36 @@
-import React, { Fragment, useState } from "react";
-import type { NextPage, GetStaticProps } from "next";
+import React, {Fragment, useState} from "react";
+import type {GetStaticProps, NextPage} from "next";
 import {
-  Box,
-  Text,
   AspectRatio,
-  LinkOverlay,
-  LinkBox,
-  Grid,
-  useMediaQuery,
-  Img,
-  Tab,
-  TabPanel,
+  Box,
   Breadcrumb,
   BreadcrumbItem,
   BreadcrumbLink,
+  Grid,
   Heading,
-  useDisclosure,
+  Img,
+  LinkBox,
+  LinkOverlay,
   Modal,
+  ModalContent,
   ModalOverlay,
-  ModalContent
+  Tab,
+  TabPanel,
+  Text,
+  useDisclosure,
+  useMediaQuery
 } from "@chakra-ui/react";
 import ExNav from "../../public/exnav";
 import Menu from "../../public/menu";
-import { tahun } from "../api/tahun";
-import type { tahun as tahunList } from "../../public/types";
-import { server } from "../../config";
-import type { gallery as galeriList } from "../../public/types";
-import { gallery } from "../api/gallery";
+import {tahun} from "../api/tahun";
+import type {gallery as galeriList, tahun as tahunList} from "../../public/types";
+import {server} from "../../config";
+import {gallery} from "../api/gallery";
 import dynamic from "next/dynamic";
+
 const PageTab = dynamic(
   () => import('../../public/pagetab'),
-  { ssr: false }
+  {ssr: false}
 )
 
 type GaleriCellProps = {
@@ -40,7 +40,7 @@ type GaleriCellProps = {
 };
 
 function GaleriCell(props: GaleriCellProps) {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const {isOpen, onOpen, onClose} = useDisclosure();
 
   return (
     <Fragment key={props.id}>
@@ -53,19 +53,19 @@ function GaleriCell(props: GaleriCellProps) {
         >
           <LinkOverlay color="teal.800">
             <AspectRatio h="100%" w="100%">
-              <Img src={`${server}/storage/${props.gambar}`} objectFit="fill" />
+              <Img src={`${server}/storage/${props.gambar}`} objectFit="fill"/>
             </AspectRatio>
           </LinkOverlay>
         </AspectRatio>
       </LinkBox>
       <Modal isOpen={isOpen} onClose={onClose} isCentered>
-        <ModalOverlay />
+        <ModalOverlay/>
         <ModalContent
-          minW={{ base: 315, xl: 720 }}
-          minH={{ base: 310, xl: 540 }}
+          minW={{base: 315, xl: 720}}
+          minH={{base: 310, xl: 540}}
         >
           <AspectRatio h="100%" w="100%">
-            <Img src={`${server}/storage/${props.gambar}`} objectFit="fill" />
+            <Img src={`${server}/storage/${props.gambar}`} objectFit="fill"/>
           </AspectRatio>
           <Box pos="absolute" bottom={0} opacity={0.6} w="100%">
             <Text
@@ -87,60 +87,60 @@ interface daftarGaleri {
   daftarGaleri: Array<galeriList>;
 }
 
-const DaftarGaleri: NextPage<daftarGaleri> = ({ daftarTahun, daftarGaleri }) => {
+const DaftarGaleri: NextPage<daftarGaleri> = ({daftarTahun, daftarGaleri}) => {
   const [galeri, setGaleri] = useState(0);
   const [isLargerThan1280] = useMediaQuery("(min-width: 1280px)");
 
   return (
     <Menu>
       <PageTab judul="Galeri"
-        breadcrumb={
-          <Breadcrumb my={{ base: "5%", xl: "80px" }} mx="6%" textColor="white" pos="absolute">
-            <Heading>Galeri</Heading>
-            <BreadcrumbItem>
-              <BreadcrumbLink href='/'>Beranda</BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbItem>
-              <BreadcrumbLink href='/galeri'>Galeri</BreadcrumbLink>
-            </BreadcrumbItem>
-          </Breadcrumb>
-        }
-        tab={
-          <React.Fragment>
-            {daftarTahun !== null &&
-              daftarTahun.map((item) => {
-                return (
-                  <Tab autoFocus onFocus={() => {
-                    setGaleri(item.tahun);
-                  }} key={item.id} w="full" justifyContent="flex-start" rounded="md" mt="0.25vw">
-                    {item.tahun}
-                  </Tab>
-                );
-              })}
-          </React.Fragment>
-        }>
+               breadcrumb={
+                 <Breadcrumb my={{base: "5%", xl: "80px"}} mx="6%" textColor="white" pos="absolute">
+                   <Heading>Galeri</Heading>
+                   <BreadcrumbItem>
+                     <BreadcrumbLink href='/'>Beranda</BreadcrumbLink>
+                   </BreadcrumbItem>
+                   <BreadcrumbItem>
+                     <BreadcrumbLink href='/galeri'>Galeri</BreadcrumbLink>
+                   </BreadcrumbItem>
+                 </Breadcrumb>
+               }
+               tab={
+                 <React.Fragment>
+                   {daftarTahun !== null &&
+                     daftarTahun.map((item) => {
+                       return (
+                         <Tab autoFocus onFocus={() => {
+                           setGaleri(item.tahun);
+                         }} key={item.id} w="full" justifyContent="flex-start" rounded="md" mt="0.25vw">
+                           {item.tahun}
+                         </Tab>
+                       );
+                     })}
+                 </React.Fragment>
+               }>
         {daftarTahun !== null &&
           daftarTahun.map((item) => {
             return (
-              <TabPanel p={0} mt={{ base: "5%", xl: 0 }} key={item.id}>
+              <TabPanel p={0} mt={{base: "5%", xl: 0}} key={item.id}>
                 {isLargerThan1280 ? (
-                  <Box w={{ xl: "68vw" }} bg="white" opacity="0.9" zIndex="999" ml={{ xl: "4%" }} p="4%">
+                  <Box w={{xl: "68vw"}} bg="white" opacity="0.9" zIndex="999" ml={{xl: "4%"}} p="4%">
                     <Grid templateColumns="repeat(6, 1fr)" gap={4}>
                       {daftarGaleri !== null &&
                         daftarGaleri.map((item) => {
                           if (item.tahun === galeri) {
-                            return <GaleriCell id={item.id} judul={item.judul} gambar={item.thumnail} />;
+                            return <GaleriCell key={item.id} id={item.id} judul={item.judul} gambar={item.thumnail}/>;
                           }
                         })}
                     </Grid>
                   </Box>
                 ) : (
-                  <Box w={{ xl: "68vw" }} bg="white" opacity="0.9" zIndex="999" ml={{ xl: "4%" }} p="4%">
+                  <Box w={{xl: "68vw"}} bg="white" opacity="0.9" zIndex="999" ml={{xl: "4%"}} p="4%">
                     <Grid gap={2}>
                       {daftarGaleri !== null &&
                         daftarGaleri.map((item) => {
                           if (item.tahun === galeri) {
-                            return <GaleriCell id={item.id} judul={item.judul} gambar={item.thumnail} />;
+                            return <GaleriCell key={item.id} id={item.id} judul={item.judul} gambar={item.thumnail}/>;
                           }
                         })}
                     </Grid>
@@ -150,7 +150,7 @@ const DaftarGaleri: NextPage<daftarGaleri> = ({ daftarTahun, daftarGaleri }) => 
             );
           })}
       </PageTab>
-      <ExNav />
+      <ExNav/>
     </Menu>
   );
 };
@@ -160,7 +160,7 @@ export const getStaticProps: GetStaticProps = async () => {
   const daftarGaleri = await gallery();
 
   return {
-    props: { daftarTahun, daftarGaleri },
+    props: {daftarTahun, daftarGaleri},
     revalidate: 15,
   };
 };
