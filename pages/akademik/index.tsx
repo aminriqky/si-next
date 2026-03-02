ef3294c8a87bb61537ee845502998a850f71388e
Adib Muhammad Maros <126374796+Adibmaros@users.noreply.github.com>
2026-03-02 22:41:38 +0700

    add tab download sebaran mata kuliah

--- pages/akademik/index.tsx
+++ pages/akademik/index.tsx
@@ -2,8 +2,12 @@ import type { NextPage, GetStaticProps } from "next";
 import React from "react";
 import {
   Box,
+  Flex,
+  Icon,
+  Link,
   Tab,
   TabPanel,
+  Text,
   BreadcrumbItem,
   BreadcrumbLink,
   Breadcrumb,
@@ -13,6 +17,8 @@ import ExNav from "../../public/exnav";
 import Menu from "../../public/menu";
 import { profil } from "../api/profil";
 import dynamic from "next/dynamic";
+import { FaExternalLinkAlt } from "@react-icons/all-files/fa/FaExternalLinkAlt";
+import { FaFileDownload } from "@react-icons/all-files/fa/FaFileDownload";
 const PageTab = dynamic(
   () => import('../../public/pagetab'),
   { ssr: false }
@@ -47,6 +53,9 @@ const Akademik: NextPage<profil> = ({ daftarProfil }) => {
             <Tab w="full" justifyContent="flex-start" rounded="md" mt="0.25vw">
               Kalender Akademik
             </Tab>
+            <Tab w="full" justifyContent="flex-start" rounded="md" mt="0.25vw">
+              Download Sebaran Mata Kuliah
+            </Tab>
           </React.Fragment>
         }>
         <TabPanel p={0} mt={{ base: "5%", xl: 0 }}>
@@ -61,6 +70,48 @@ const Akademik: NextPage<profil> = ({ daftarProfil }) => {
             <Box fontSize={{ base: "xs", lg: "md" }}>
               <div dangerouslySetInnerHTML={{ __html: daftarProfil[3].text }} />
             </Box>
+          </Box>
+        </TabPanel>
+        <TabPanel p={0} mt={{ base: "5%", xl: 0 }}>
+          <Box w={{ xl: "68vw" }} bg="white" opacity="0.9" zIndex="999" ml={{ xl: "4%" }} p="4%">
+            <Text textColor="black" fontSize="2xl" fontWeight="semibold" mb="4">
+              Sebaran Mata Kuliah
+            </Text>
+            <Text fontSize={{ base: "sm", lg: "md" }} color="gray.600" mb="4">
+              Download file sebaran mata kuliah Program Studi Sistem Informasi melalui link berikut:
+            </Text>
+            <Link
+              href="https://drive.google.com/drive/folders/1UkcnDPDyf4P1ar-XFZhO5nBm9WnUTlo-?usp=sharing"
+              isExternal
+              _hover={{ textDecor: "none" }}
+            >
+              <Flex
+                p="4%"
+                bg="blue.50"
+                border="1px"
+                borderColor="blue.200"
+                borderRadius="md"
+                alignItems="center"
+                _hover={{ bg: "blue.100" }}
+              >
+                <Icon
+                  color="blue.500"
+                  as={FaFileDownload}
+                  w="24px"
+                  h="auto"
+                  mr="15px"
+                />
+                <Box>
+                  <Text fontWeight="semibold" color="blue.700">
+                    Sebaran Mata Kuliah - Google Drive
+                  </Text>
+                  <Text color="blue.500" fontSize="sm">
+                    Klik untuk membuka folder Google Drive
+                    <Icon as={FaExternalLinkAlt} mx="6px" w="12px" h="auto" />
+                  </Text>
+                </Box>
+              </Flex>
+            </Link>
           </Box>
         </TabPanel>
       </PageTab>
