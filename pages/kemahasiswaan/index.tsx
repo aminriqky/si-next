c8f40416f201894057e13c02a037fa378bca059a
Adib Muhammad Maros <126374796+Adibmaros@users.noreply.github.com>
2026-03-02 23:04:52 +0700

    Refactor import statements and formatting in index.tsx

--- pages/kemahasiswaan/index.tsx
+++ pages/kemahasiswaan/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import type {GetStaticProps, NextPage} from "next";
+import type { GetStaticProps, NextPage } from "next";
 import {
   Box,
   Breadcrumb,
@@ -15,15 +15,15 @@ import {
 } from "@chakra-ui/react";
 import ExNav from "../../public/exnav";
 import Menu from "../../public/menu";
-import {organisasi} from "../api/organisasi";
-import {profil} from "../api/profil";
-import {server} from "../../config";
-import type {organisasi as orgList, profil as profilList} from "../../public/types";
+import { organisasi } from "../api/organisasi";
+import { profil } from "../api/profil";
+import { server } from "../../config";
+import type { organisasi as orgList, profil as profilList } from "../../public/types";
 import dynamic from "next/dynamic";
 
 const PageTab = dynamic(
   () => import('../../public/pagetab'),
-  {ssr: false}
+  { ssr: false }
 )
 
 type MahasiswaCellProps = {
@@ -35,23 +35,23 @@ type MahasiswaCellProps = {
 const MahasiswaCell: React.FC<MahasiswaCellProps> = (props) => {
   return (
     <React.Fragment>
-      <Flex flexDir={{base: "column", xl: "row"}} mb="2%">
+      <Flex flexDir={{ base: "column", xl: "row" }} mb="2%">
         <Img
           src={`${server}/storage/${props.logo}`}
           borderRadius="5"
           maxW="320px"
           objectFit="contain"
         />
-        <Flex ml={{base: 0, xl: 32}} flexDir="column" flexWrap="wrap">
+        <Flex ml={{ base: 0, xl: 32 }} flexDir="column" flexWrap="wrap">
           <Text mb="10px" color={props.color} fontWeight="semibold">
             {props.children}
           </Text>
           <Box
             color="black"
             fontWeight="light"
-            fontSize={{base: "xs", lg: "md"}}
+            fontSize={{ base: "xs", lg: "md" }}
           >
-            <div dangerouslySetInnerHTML={{__html: props.detail}}/>
+            <div dangerouslySetInnerHTML={{ __html: props.detail }} />
           </Box>
         </Flex>
       </Flex>
@@ -72,83 +72,68 @@ const DaftarKemahasiswaan: NextPage<daftarKemahasiswaan> = ({
   return (
     <Menu>
       <PageTab judul="Kemahasiswaan"
-               breadcrumb={
-                 <Breadcrumb my={{base: "5%", xl: "80px"}} mx="6%" textColor="white" pos="absolute">
-                   <Heading>Kemahasiswaan</Heading>
-                   <BreadcrumbItem>
-                     <BreadcrumbLink href='/'>Beranda</BreadcrumbLink>
-                   </BreadcrumbItem>
-                   <BreadcrumbItem>
-                     <BreadcrumbLink href='/kemahasiswaan'>Kemahasiswaan</BreadcrumbLink>
-                   </BreadcrumbItem>
-                 </Breadcrumb>
-               }
-               tab={
-                 <React.Fragment>
-                   <Tab w="full" justifyContent="flex-start" rounded="md" mt="0.25vw">
-                     Himpunan Mahasiswa
-                   </Tab>
-                   <Tab w="full" justifyContent="flex-start" rounded="md" mt="0.25vw">
-                     Organisasi Mahasiswa
-                   </Tab>
-                   <Tab w="full" justifyContent="flex-start" rounded="md" mt="0.25vw">
-                     Kehidupan Mahasiswa
-                   </Tab>
-                   <Tab w="full" justifyContent="flex-start" rounded="md" mt="0.25vw">
-                     Survei Mahasiswa
-                   </Tab>
-                 </React.Fragment>
-               }>
+        breadcrumb={
+          <Breadcrumb my={{ base: "5%", xl: "80px" }} mx="6%" textColor="white" pos="absolute">
+            <Heading>Kemahasiswaan</Heading>
+            <BreadcrumbItem>
+              <BreadcrumbLink href='/'>Beranda</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbItem>
+              <BreadcrumbLink href='/kemahasiswaan'>Kemahasiswaan</BreadcrumbLink>
+            </BreadcrumbItem>
+          </Breadcrumb>
+        }
+        tab={
+          <React.Fragment>
+            <Tab w="full" justifyContent="flex-start" rounded="md" mt="0.25vw">
+              Organisasi Mahasiswa
+            </Tab>
+            <Tab w="full" justifyContent="flex-start" rounded="md" mt="0.25vw">
+              Kehidupan Mahasiswa
+            </Tab>
+            <Tab w="full" justifyContent="flex-start" rounded="md" mt="0.25vw">
+              Survei Mahasiswa
+            </Tab>
+          </React.Fragment>
+        }>
         <TabPanels>
-          <TabPanel p={0} mt={{base: "5%", xl: 0}}>
-            <Box w={{xl: "68vw"}} bg="white" opacity="0.9" zIndex="999" ml={{xl: "4%"}} p="4%">
-              <Box fontSize={{base: "xs", lg: "md"}}>
-                <Text textColor="black" fontSize="2xl" fontWeight="semibold" mb="6">
-                  Himpunan Mahasiswa
-                </Text>
-                <MahasiswaCell
-                  color="black"
-                  detail={daftarOrganisasi[1].detail}
-                  logo={daftarOrganisasi[1].logo}
-                >
-                  {daftarOrganisasi[1].judul}
-                </MahasiswaCell>
-              </Box>
-            </Box>
-          </TabPanel>
-          <TabPanel p={0} mt={{base: "5%", xl: 0}}>
-            <Box w={{xl: "68vw"}} bg="white" opacity="0.9" zIndex="999" ml={{xl: "4%"}} p="4%">
-              <Box fontSize={{base: "xs", lg: "md"}}>
+          <TabPanel p={0} mt={{ base: "5%", xl: 0 }}>
+            <Box w={{ xl: "68vw" }} bg="white" opacity="0.9" zIndex="999" ml={{ xl: "4%" }} p="4%">
+              <Box fontSize={{ base: "xs", lg: "md" }}>
                 <Text textColor="black" fontSize="2xl" fontWeight="semibold" mb="6">
                   Organisasi Mahasiswa
                 </Text>
-                <MahasiswaCell
-                  color="black"
-                  detail={daftarOrganisasi[0].detail}
-                  logo={daftarOrganisasi[0].logo}
-                >
-                  {daftarOrganisasi[0].judul}
-                </MahasiswaCell>
+                {daftarOrganisasi !== null &&
+                  daftarOrganisasi.map((item) => (
+                    <MahasiswaCell
+                      key={item.id}
+                      color="black"
+                      detail={item.detail}
+                      logo={item.logo}
+                    >
+                      {item.judul}
+                    </MahasiswaCell>
+                  ))}
               </Box>
             </Box>
           </TabPanel>
-          <TabPanel p={0} mt={{base: "5%", xl: 0}}>
-            <Box w={{xl: "68vw"}} bg="white" opacity="0.9" zIndex="999" ml={{xl: "4%"}} p="4%">
-              <Box fontSize={{base: "xs", lg: "md"}}>
-                <div dangerouslySetInnerHTML={{__html: daftarProfil[21].text}}/>
+          <TabPanel p={0} mt={{ base: "5%", xl: 0 }}>
+            <Box w={{ xl: "68vw" }} bg="white" opacity="0.9" zIndex="999" ml={{ xl: "4%" }} p="4%">
+              <Box fontSize={{ base: "xs", lg: "md" }}>
+                <div dangerouslySetInnerHTML={{ __html: daftarProfil[21]?.text ?? "" }} />
               </Box>
             </Box>
           </TabPanel>
-          <TabPanel p={0} mt={{base: "5%", xl: 0}}>
-            <Box w={{xl: "68vw"}} bg="white" opacity="0.9" zIndex="999" ml={{xl: "4%"}} p="4%">
-              <Box fontSize={{base: "xs", lg: "md"}}>
-                <div dangerouslySetInnerHTML={{__html: daftarProfil[22].text}}/>
+          <TabPanel p={0} mt={{ base: "5%", xl: 0 }}>
+            <Box w={{ xl: "68vw" }} bg="white" opacity="0.9" zIndex="999" ml={{ xl: "4%" }} p="4%">
+              <Box fontSize={{ base: "xs", lg: "md" }}>
+                <div dangerouslySetInnerHTML={{ __html: daftarProfil[22]?.text ?? "" }} />
               </Box>
             </Box>
           </TabPanel>
         </TabPanels>
       </PageTab>
-      <ExNav/>
+      <ExNav />
     </Menu>
   );
 };
@@ -158,7 +143,7 @@ export const getStaticProps: GetStaticProps = async () => {
   const daftarProfil = await profil();
 
   return {
-    props: {daftarOrganisasi, daftarProfil},
+    props: { daftarOrganisasi, daftarProfil },
     revalidate: 15,
   };
 };
